08221c5dba1183713cd4bd4eda4b7d52968ab6f5
RowdyOnline <rowdy14@gmail.com>
2020-12-01 15:33:02 -0800

    updated

--- functions_int1.py
+++ functions_int1.py
@@ -1,10 +1,10 @@
 import random
 
-def randint(min=0, max=100):
+def randint(min=50, max=10):
     if min > max:
         return 'Min has to be less than Max'
     else:
         num = random.random() * (max-min) + min
         return num
 
-print(randint(min=50, max=5))
+print(randint(min=-50, max=-20))
