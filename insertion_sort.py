cf96b247b5b6b92133a8c98aeb8df9e776ad94e9
RowdyOnline <rowdy14@gmail.com>
2020-12-02 13:33:50 -0800

    Coding Dojo insertion sort solution added

--- insertion_sort.py
+++ insertion_sort.py
@@ -15,3 +15,19 @@ def insertion_sort(new_list):
 print("Complete")
 
 insertion_sort(new_list)
+
+#Solution from Coding Dojo
+# my_arr = [12,3,6,2,3,5,6,7,8]*400
+
+# def dojo_insert(my_arr):
+#     for idx in range(1, len(my_arr)): 
+#         temp = my_arr[idx] 
+#         prev_idx = idx-1
+#         while prev_idx >= 0 and temp < my_arr[prev_idx]: 
+#             my_arr[prev_idx + 1] = my_arr[prev_idx] 
+#             prev_idx -= 1
+#         my_arr[prev_idx + 1] = temp 
+#     return my_arr
+# print("Complete")
+
+# dojo_insert(my_arr)
