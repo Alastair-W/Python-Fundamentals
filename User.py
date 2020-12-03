02cf50adab7c809f9e689992de227340b4caa5f1
RowdyOnline <rowdy14@gmail.com>
2020-12-03 12:19:15 -0800

    update

--- User.py
+++ User.py
@@ -9,16 +9,20 @@ class User:		# here's what we have so far
             self.account_balance += amount	# the specific user's account increases by the amount of the value received
         else:
             print("ERROR: Please retry using a positive number")
+        return self
     def make_withdrawal(self, amount):
         if amount > 0:
             self.account_balance -= amount
         else:
             print("ERROR: Please retry using a positive number")
+        return self
     def display_user_balance(self):
-        return f"User: {self.name}, Balance: {self.account_balance}"
+        print(f"User: {self.name}, Balance: {self.account_balance}")
+        return self
     def transfer_money(self, other_user, amount):
         self.make_withdrawal(amount)
         other_user.make_deposit(amount)
+        return self
         
 
 
@@ -29,12 +33,16 @@ monty = User("Monty Python", "monty@python.com")
 guido.make_deposit(100)
 guido.make_deposit(200)
 monty.make_deposit(50)
-print(guido.account_balance)	# output: 300
-print(monty.account_balance)	# output: 50
+guido.account_balance	# output: 300
+monty.account_balance	# output: 50
 guido.make_withdrawal(20)
-print(guido.display_user_balance())	# output: Guido, 280
+guido.display_user_balance()	# output: Guido, 280
 guido.transfer_money(monty, 25)
-print(guido.display_user_balance())
-print(monty.display_user_balance())
-monty.make_deposit(-10)
-print(monty.display_user_balance())
+guido.display_user_balance()
+monty.display_user_balance()
+monty.make_deposit(10)
+monty.display_user_balance()
+
+# Rewritten using Chaining Methods approach with 'return self' aded to every method in the Class
+
+guido.make_deposit(100).make_deposit(100).make_withdrawal(20).display_user_balance().transfer_money(monty, 400).display_user_balance()
