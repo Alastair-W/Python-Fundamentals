160a4397b189afc499aef6b54a8152c9c4cde8d6
RowdyOnline <rowdy14@gmail.com>
2020-12-04 11:56:35 -0800

    Updated class inheritance BankAccount.py

--- BankAccount.py
+++ BankAccount.py
@@ -2,60 +2,54 @@ class User:		# here's what we have so far
     def __init__(self, name, email):
         self.name = name
         self.email = email
-        self.account = BankAccount(int_rate=0.02, balance=0)
-    # updating all transactional methods from User class to BankAccount class
-    def make_deposit(self, amount):	# takes an argument that is the amount of the deposit
-        if amount > 0:
-            self.account.deposit(amount)	# the specific user's account increases by the amount of the value received
-            print(f"Balance: ${self.account.balance}")
-        else:
-            print("ERROR: Please retry using a positive number")
-        return self
-    def make_withdrawal(self, amount):
-        if amount > 0:
-            self.account.withdrawal(amount)
-            print(f"Balance: ${self.account.balance}")
-        else:
-            print("ERROR: Please retry using a positive number")
-        return self
-    def display_user_balance(self):
-        self.account.display_account_info()
-        return self
-    def transfer_money(self, other_user, amount):
-        self.account.withdrawal(amount)
-        other_user.account.deposit(amount)
-        return self
+    def printUser(self):
+        print(self.name)
+    
 
-class BankAccount:
-    def __init__ (self, int_rate, balance=0):
+class BankAccount(User):
+    def __init__ (self, name, email, acc_name='Checking'):
+        User.__init__(self, name, email)
         self.int_rate = 0.01
-        self.balance = balance
+        self.balance = 50
+        self.acc_name = acc_name
+        print(f'Name: {self.name}, Account Type: {self.acc_name}, Balance: ${self.balance}')
     def deposit(self, amount):
         self.balance += amount
+        self.display_account_info()
         return self
     def withdrawal(self, amount):
         if self.balance >= amount:
             self.balance -= amount
+            self.display_account_info()
         else:
             self.balance -= 5
             print("Sorry you have insufficient funds and will be charged $5")
+            self.display_account_info()
         return self
     def display_account_info(self):
-        print(f"Balance: ${self.balance}")
+        print(f"Name: {self.name}, Account Type: {self.acc_name}, Balance: ${self.balance}")
         return self
     def yield_interest(self):
         interest = self.balance * self.int_rate
         print(f"Interest: ${interest}")
         return self
 
-    
-# firstAccount = BankAccount(0.05, 200)
-# secondAccount = BankAccount(0.03, 150)
-
-# firstAccount.deposit(200).deposit(200).deposit(200).withdrawal(900).yield_interest().display_account_info()
-# secondAccount.deposit(200).deposit(200).withdrawal(50).withdrawal(30).withdrawal(50).withdrawal(30).yield_interest().display_account_info()
-
+# Test to see if the Class Inheritance is set up correctly
+print(issubclass(BankAccount, User))
+# Set up a new User
 guido = User('Guido', 'guido@email.com')
+# Set up two new Accounts using the properties from User class
+guidoCheck = BankAccount('Guido', 'guido@email.com')
+guidoSave = BankAccount('Guido', 'guido@email.com', acc_name = 'Savings')
+# Test out methods
+guidoSave.deposit(50)
+guidoCheck.withdrawal(25)
+guidoSave.display_account_info()
+guidoSave.yield_interest()
+guidoSave.withdrawal(80)
+# Chained
+guidoSave.deposit(50).display_account_info().yield_interest().withdrawal(80)
 
 
-guido.make_deposit(-100).make_withdrawal(10).display_user_balance()
+
+
