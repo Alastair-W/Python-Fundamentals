14b116ffb8097b5342cc4043bead7168f2f341d1
RowdyOnline <rowdy14@gmail.com>
2020-12-08 12:59:28 -0800

    Stores and Products complete

--- Store_Products/store_prod.py
+++ Store_Products/store_prod.py
@@ -3,17 +3,33 @@ class Store:
         self.name = name
         self.list = []
     def add_product(self, new_product):
-        self.new_product = new_product
-        self.list.append(self.new_product)
+        self.list.append(new_product)
         return self
-    def sell_product(self, id):
-        self.id = id
-        self.list.remove(self.list[id])
-        print(self.list[id])
+    # def find_index(self, prod):
+    #     count = 0
+    #     for t in self.list:
+    #         count += 1
+    #         if t.name == prod:
+    #             print(f'{prod} is at location {count}')
+    def sell_product(self, prod):
+        # print(self.list[id])
+        count = -1
+        for t in self.list:
+            count += 1
+            if t.name == prod:
+                print(f'{prod} is at location {count}')
+                self.list.remove(self.list[count])
+                print(f'{prod} is now removed from your list')
+                self.see_list()
         return self
     def inflation(self, percent_increase):
-        self.percent_increase = percent_increase
-        Product.update_price(self.percent_increase, is_increased=True)
+        for i in self.list:
+            i.update_price(percent_increase, is_increased=True)
+    def see_list(self):
+        temp_list = []
+        for item in self.list:
+            temp_list.append(item.name)
+        print(f'Here is your current list of products: {temp_list}')
     
 
 class Product:
@@ -22,32 +38,54 @@ class Product:
         self.price = price
         self.category = category
     def update_price(self, percent_change, is_increased):
-        self.percent_change = percent_change
-        self.is_increased = is_increased
         if is_increased == True:
-            self.price += self.price*percent_change
+            self.price += round(self.price*percent_change, 2)
         else:
-            self.price -= self.price*percent_change
+            self.price -= round(self.price*percent_change, 2)
         self.print_info()
         return self
     def print_info(self):
         print(f'Product Name: {self.name}, Category: {self.category}, Price: {self.price}')
         return self
 
+# Create instance of Product class
+p = Product('banana', 1.50, 'fruit')
 
-p = Product('banana',1.50, 'fruit')
+# Call function in Product class printing Instance Info
 p.print_info()
+
+# Create instance of Store class
 s = Store('Safeway')
-s.add_product(p.name)
+
+# Call function in Product class that updates price and prints new info
 p.update_price(0.25, True)
+
+# Call function in Store class that adds instance of Product class to list 
+s.add_product(p)
+
+# Print list to verify it worked
+s.see_list()
+
+# Create 4 more instances of Product class 
 q = Product('milk', 3.99, 'dairy')
 r = Product('eggs', 7.99, 'dairy')
 d = Product('soda', 3.50, 'drink')
 m = Product('mushrooms', 7.56, 'produce')
-print(s.list)
-s.add_product(q.name).add_product(r.name).add_product(d.name).add_product(m.name)
-print(s.list)
-print(s.list[2])
-s.sell_product(2)
-print(s.list)
-# s.inflation(0.5)
+
+# Call function in Store class that adds all new instances of Product class to list
+s.add_product(q).add_product(r).add_product(d).add_product(m)
+
+# Print list to verify it worked
+s.see_list()
+
+# Call function in Product class that updates price and prints new info
+p.update_price(0.2, True)
+
+# Call function that sells the product and removes it from the Store list
+s.sell_product('soda')
+
+# Call function that increases all prices
+s.inflation(0.15)
+
+s.see_list()
+# print(p.__dict__)
