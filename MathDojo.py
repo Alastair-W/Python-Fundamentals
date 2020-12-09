ecffaa5e9c8e5919d691675487b47d856f79f3ed
RowdyOnline <rowdy14@gmail.com>
2020-12-09 15:13:41 -0800

    MathDojo.py-  use SetUp method for class instance

--- MathDojo.py
+++ MathDojo.py
@@ -22,7 +22,9 @@ class MathDojo:
                     self.result -= n
         return self
 
-
+# p = MathDojo()
+# p.subtract(12,225,10.1)
+# print(p.result)
 # create an instance:
 # md = MathDojo()
 # # to test:
@@ -32,20 +34,22 @@ class MathDojo:
 
 
 class mathDojoTest(unittest.TestCase):
-    def testAddition(self):
-        self.assertGreater(MathDojo().add(2,5,1).result, 0)
-        self.assertEqual(MathDojo().add(2,5,1).result, 8)
-        self.assertEqual(MathDojo().add(21,25,10).result, 56)
-        self.assertEqual(MathDojo().add(112,225,10.1).result, 347.1)
-    def testSubtraction(self):
-        self.assertLess(MathDojo().subtract(3,2,1).result, -5)
-        self.assertEqual(MathDojo().subtract(12,225,10.1).result, -247.1)
-    def testNegInt(self):
-        self.assertFalse(MathDojo().subtract(-3,2,1))
-        self.assertFalse(MathDojo().subtract(3,-2,1))
-        
     def setUp(self):
+        self.md = MathDojo()
         print('running setUp')
+    def testAddition(self):
+        self.assertGreater(self.md.add(2,5,1).result, 0)
+        self.assertEqual(self.md.add(2,5,1).result, 8)
+        self.assertEqual(self.md.add(21,25,10).result, 56)
+        self.assertEqual(self.md.add(112,225,10.1).result, 347.1)
+    def testSubtraction1(self):
+        self.assertLess(self.md.subtract(3,2,1).result, -5)
+    def testSubtraction2(self):
+        self.assertEqual(self.md.subtract(12,225,10.1).result, -247.1)
+    def testNegInt(self):
+        self.assertFalse(self.md.subtract(-3,2,1))
+        self.assertFalse(self.md.subtract(3,-2,1))
+    
     def tearDown(self):
         print('running tearDown task')
 
