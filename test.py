b130cb9fb68171fb7491bb3a869577013564460d
RowdyOnline <rowdy14@gmail.com>
2020-12-09 15:03:40 -0800

    MathDojo.py complete\

--- test.py
+++ test.py
@@ -1,66 +1,53 @@
 import unittest
-import math
 
-
-def coins(money):
-    if money == 0:
-        print("That is the exact amount so there is no change!")
-        return False
-    elif money < 0:
-        balance = money * -1
-        print(f'Ummm, well this is awkward - you still owe me 0.{balance} cents ...')
-        return False
-    else:
-        newList = []
-        if money % 25 == 0:
-            q = math.floor(money / 25)
-            newList.extend((q,0,0,0))
+class MathDojo:
+    def __init__(self):
+        self.result = 0
+    def add(self, num, *nums):
+        self.result = num
+        for n in nums:
+            self.result += n
+        return self
+    def subtract(self, num, *nums):
+        if num < 0:
+            print('Please type a positive integer')
+            return False
         else:
-            q = math.floor(money / 25)
-            newList.append(q)
-            money %= 25
-            if money % 10 == 0:
-                d = math.floor(money / 10)
-                newList.extend((d,0,0))
-            else:
-                d = math.floor(money / 10)
-                newList.append(d)
-                money %= 10
-                if money % 5 == 0:
-                    n = math.floor(money / 5)
-                    newList.extend((n,0))
+            self.result -= num
+            for n in nums:
+                if n < 0:
+                    print('Please only type in a positive integer')
+                    return False
                 else:
-                    n = math.floor(money / 5)
-                    newList.append(n)
-                    money %= 5
-                    newList.append(money)
-        return newList
-
-print(coins(87))
-print(coins(30))
-print(coins(1267))
-print(coins(20))
-print(coins(1))
-print(coins(0))
-print(coins(-43))
+                    self.result -= n
+        return self
 
 
-class coinsTest(unittest.TestCase):
-    def calculations(self):
-        self.assertEqual(coins(87), [3,1,0,2])
-        self.assertEqual(coins(30), [1,0,1,0])
-        self.assertEqual(coins(1267), [50,1,1,2])
-        self.assertEqual(coins(20), [0,2,0,0])
-        self.assertEqual(coins(1), [0,0,0,1])
-    def errors(self):
-        self.assertFalse(coins(0))
-        self.assertFalse(coins(-43))
+# create an instance:
+# md = MathDojo()
+# # to test:
+# x = md.add(2).add(2,5,1).subtract(3,2).result
+# print(x)	# should print 5
+# run each of the methods a few more times and check the result!
+
+
+class mathDojoTest(unittest.TestCase):
+    def testAddition(self):
+        self.assertGreater(MathDojo().add(2,5,1).result, 0)
+        self.assertEqual(MathDojo().add(2,5,1).result, 8)
+        self.assertEqual(MathDojo().add(21,25,10).result, 56)
+        self.assertEqual(MathDojo().add(112,225,10.1).result, 347.1)
+    def testSubtraction(self):
+        self.assertLess(MathDojo().subtract(3,2,1).result, -5)
+        self.assertEqual(MathDojo().subtract(12,225,10.1).result, -247.1)
+    def testNegInt(self):
+        self.assertFalse(MathDojo().subtract(-3,2,1))
+        self.assertFalse(MathDojo().subtract(3,-2,1))
+        
     def setUp(self):
-        print("running setUp")
-    # any task you want run after the tests are executed, put them in the tearDown method
+        print('running setUp')
     def tearDown(self):
-    # add the tearDown tasks
-        print("running tearDown tasks")
+        print('running tearDown task')
 
 if __name__ == '__main__':
-    unittest.main() # this runs our tests
+    unittest.main()
