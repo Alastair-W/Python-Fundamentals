c8e36c6a39c62a13d72c37fa90f3d6d299338cef
RowdyOnline <rowdy14@gmail.com>
2020-12-17 08:48:44 -0800

    update

--- Data_structures/unit_test.py
+++ Data_structures/unit_test.py
@@ -1,1 +1,10 @@
 import unittest
+import testing
+
+class linkedListTest(unittest.TestCase):
+    def setUp(self):
+        self.my_list = SLList()
+    def testAdd(self):
+        self.assertEqual(addToFront('Jim'), )
+
+    def tearDown(self):
